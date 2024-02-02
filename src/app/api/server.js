a56e666373fa21380dc05298799488187d8929ee
Julian Kisner <julikisner@gmail.com>
2024-02-02 12:02:50 -0300

    change url cors

--- src/app/api/server.js
+++ src/app/api/server.js
@@ -30,7 +30,7 @@ async function obtenerUserIdDeInicoSesion(socket) {
 
 const io = new Server(httpServer, {
   cors: {
-    origin: ["https://packar-it.vercel.app" || "http://localhost:3000"],
+    origin: "https://packar-it-9i79.vercel.app",
     methods: ["GET", "POST"],
   },
 });
